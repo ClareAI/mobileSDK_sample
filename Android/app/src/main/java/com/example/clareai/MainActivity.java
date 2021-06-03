134c4f0ac350dfdfc766604dc23529cfb43b9b40
Ying Yu <vernal@clare.ai>
2021-06-03 22:29:08 +0800

    Android Sample Setup with new SDK 3.4.4

--- Android/app/src/main/java/com/example/clareai/MainActivity.java
+++ Android/app/src/main/java/com/example/clareai/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         btnShowWithoutIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Clare.showWithoutWidget(MainActivity.this.getFragmentManager());
+                Clare.showWithoutWidget(MainActivity.this.getSupportFragmentManager());
             }
         });
 
